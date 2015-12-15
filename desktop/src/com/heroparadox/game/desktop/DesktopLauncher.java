6bcf3c11fe7462bccfb4cd1e198bb41f93850fdf
rainy <rainy@DESKTOP-PA8C44T>
2015-12-15 10:24:27 -0500

    Merge origin/master
    Conflicts:
    	src/hhssadventure/AdventureInterface.form
    	src/hhssadventure/AdventureInterface.java
    	src/hhssadventure/HHSSAdventure.java

---
+++ desktop/src/com/heroparadox/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.heroparadox.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.heroparadox.game.TheGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheGdxGame(), config);
+	}
+}
