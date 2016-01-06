8ff60f6f5edb54590713ae81ab875f221acdc48f
janaj4926 <janaj4926@HRH-IC0027341.wrdsb.ca>
2016-01-06 11:10:19 -0500

    Asset manager and pictures.

--- core/src/Screens/AssetManager.java
+++ core/src/Screens/AssetManager.java
@@ -15,19 +15,30 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class AssetManager {
     private static TextureAtlas atlas;
     
+    //bosses
     public static TextureRegion GolemBoss;
     public static TextureRegion KingBoss;
     public static TextureRegion KnightBoss;
-    public static TextureRegion Player;
     public static TextureRegion TurtleBoss;
+    
+    //floors
     public static TextureRegion DirtFloor;
     public static TextureRegion SandFloor;
     public static TextureRegion CastleFloor;
     
+    //player
+    public static TextureRegion PlayerStand;
     public static Animation PlayerWalk;
     public static Animation PlayerWalkL;
     
+    //background
+    public static TextureRegion background;
+    
     public static void load(){
-        atlas = new TextureAtlas("Player.pack");
+        atlas = new TextureAtlas("Pictures for project.pack");
+        PlayerStand = atlas.findRegion("white.png");
+        DirtFloor = atlas.findRegion("Dirt.png");
+        background = atlas.findRegion("background.png");
+        
     }
 }
