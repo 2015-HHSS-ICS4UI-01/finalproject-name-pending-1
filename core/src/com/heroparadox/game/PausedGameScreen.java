54907d89497f469faeed6854dfb4b8fc9aa2f0fd
tituo4996 <tituo4996@HRH-IC0027340.wrdsb.ca>
2016-01-13 11:20:23 -0500

    Saving to the cloud

--- core/src/com/heroparadox/game/PausedGameScreen.java
+++ core/src/com/heroparadox/game/PausedGameScreen.java
@@ -9,58 +9,80 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  *
  * @author tituo4996
  */
 public class PausedGameScreen implements Screen {
+GdxGame game; // Note it's "MyGame" not "Game"
+ 
+        // my games virtual width and height
+        public final int V_WIDTH = 1024;
+        public final int V_HEIGHT = 1280;
 
-    GdxGame game;
+        private Viewport viewport;
+        private OrthographicCamera camera;
+        private SpriteBatch batch;
+        private Texture logo;
     
-    public PausedGameScreen(GdxGame game){
-        this.game = game;
-    }
     
-    @Override
-    public void render(float delta) {
-        //render the paused screen picture
-        if(Gdx.input.isKeyJustPressed(Keys.ESCAPE)){
-            game.setScreen(game.mainMenuScreen);
-        }else if(Gdx.input.isKeyJustPressed(Keys.SPACE)){
-            game.setScreen(game.mainGame);
+
+        public PausedGameScreen(GdxGame game){
+            this.game = game;
+            camera = new OrthographicCamera();
+            viewport = new FitViewport(V_WIDTH, V_HEIGHT, camera);
+            logo = new Texture("pauseScreen.png");
         }
         
-    }
+        @Override
+        public void render(float delta) {
+            //render the main menu picture
+             if (Gdx.input.isButtonPressed(Keys.SPACE)) 
+                 game.setScreen(game.mainGame);
+             else if (Gdx.input.isButtonPressed(Keys.ESCAPE)) 
+                 game.setScreen(game.mainMenuScreen);
+             
+             
+             
+                 
+        }
+ 
 
-    @Override
-    public void resize(int i, int i1) {
-        
-    }
+       @Override
+        public void resize(int width, int height) {
+        }
+ 
 
-    @Override
-    public void pause() {
-        
-    }
+       @Override
+        public void show() {
+             // called when this screen is set as the screen with game.setScreen();
+        }
+ 
 
-    @Override
-    public void resume() {
-        
-    }
+       @Override
+        public void hide() {
+             // called when current screen changes from this to a different screen
+        }
+ 
 
-    @Override
-    public void hide() {
-        
-    }
+       @Override
+        public void pause() {
+        }
+ 
 
-    @Override
-    public void dispose() {
-        
-    }
+       @Override
+        public void resume() {
+        }
+ 
 
-    @Override
-    public void show() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+       @Override
+        public void dispose() {
+                // never called automatically
+        }
 }
