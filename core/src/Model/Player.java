639ea6aa8e95dade46436fb46a216a6d504a23c5
tituo4996 <tituo4996@HRH-IC0027340.wrdsb.ca>
2016-01-18 11:21:00 -0500

    Saving to the cloud

--- core/src/Model/Player.java
+++ core/src/Model/Player.java
@@ -33,8 +33,8 @@ public class Player extends Entity {
     }
 
     public void update(float delta) {
-//        acceleration.y = -9.8f;
-        
+        acceleration.y = -9.8f;
+      
         velocity.mulAdd(acceleration, delta);
         
         velocity.x = velocity.x * DAMP;
