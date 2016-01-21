14cc912e727e9a4c95cfd66fa6c31b0a49c9c194
janaj4926 <janaj4926@HRH-IC0027341.wrdsb.ca>
2016-01-21 11:20:18 -0500

    sword

---
+++ core/src/Model/Sword.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author janaj4926
+ */
+public class Sword extends Entity{
+    public Sword(float x, float y, float width, float height) {
+        super(x,y,width,height);
+    }
+    
+}
